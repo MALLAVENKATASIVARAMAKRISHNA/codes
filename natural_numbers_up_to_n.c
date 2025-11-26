bb71ead2230bb783908977270ea55d6e87abef40
ramakrihsna <2003ramakrishna@gmail.com>
2025-11-26 10:19:10 +0530

    c program to print natural numbers up to n

---
+++ natural_numbers_up_to_n.c
@@ -0,0 +1,31 @@
+/*step-1: start
+step-2: print "Up to which number you want to print natural numbers? "
+step-3: read Count
+step-4: Counter = 1
+step-5: print "The first " + Count + " natural numbers are "
+step-6: if Counter < Count:
+          print Counter + ", "
+          Counter = Counter + 1
+          goto step-6
+step-7: print Counter + ".\n"
+step-8: stop*/
+
+#include <stdio.h>
+
+int main()
+{
+    int count, counter;
+
+    printf("Up to which number you want to print natural numbers? ");
+    scanf("%i", count);
+    printf("The first %i natural numbers are ", count);
+    counter = 1;
+    while (counter < count)
+    {
+        printf("%i, ", counter)
+        counter = counter + 1;
+    }
+    printf("%i.\n", counter);
+
+    return 0;
+}
